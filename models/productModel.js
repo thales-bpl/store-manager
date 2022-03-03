2680705edcc84d989cc94f2c6189946881936a6e
thales-bpl <thales.bplima@gmail.com>
2022-03-03 03:26:17 -0300

    fix model.getProduct ORDER BY

--- models/productModel.js
+++ models/productModel.js
@@ -1,13 +1,16 @@
 const connection = require('./connection');
 
 const getProducts = async () => {
-  const query = 'SELECT * FROM StoreManager.products;';
+  const query = `SELECT * FROM StoreManager.products
+    ORDER BY id;`;
   const [rows] = await connection.execute(query);
   return rows;
 };
 
 const getProductById = async (id) => {
-  const query = 'SELECT * FROM StoreManager.products WHERE id = ?;';
+  const query = `SELECT * FROM StoreManager.products
+    WHERE id = ?
+    ORDER BY id;`;
   const [rows] = await connection.execute(query, [id]);
   return rows;
 };
