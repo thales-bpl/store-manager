c84597e0c3abb625566dc585de51aaba2573c871
thales-bpl <thales.bplima@gmail.com>
2022-03-08 02:56:22 -0300

    tryfix GH avaliator

--- models/connection.js
+++ models/connection.js
@@ -5,6 +5,7 @@ const connection = mysql.createPool({
   host: process.env.MYSQL_HOST,
   user: process.env.MYSQL_USER,
   password: process.env.MYSQL_PASSWORD,
+  port: 3306,
 });
 
 module.exports = connection;
